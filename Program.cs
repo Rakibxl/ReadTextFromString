43ae9b4188beaf2efb3c7032a7fa7a12a6c7e146
Rakib Ahmed <rakib.ahmed@econocom.com>
2020-07-08 18:42:46 +0200

    Added new logic to improve extracttion

--- Program.cs
+++ Program.cs
@@ -1,5 +1,169 @@
-﻿using System;
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 
@@ -89,7 +253,7 @@ public class Program
         SamsungOrder samsungOrder = new SamsungOrder();
 
 
-        var orderTxt = @"Purchase Order PO No : 5299649779 Date : 2020.06.25 Supplier Name & Address Buyer Name & Address L10IU1 : ECONOCOM INTERNATIONAL ITALIA S.P.A Samsung Electronics Italia S.p.A Via Marcello Nizzoli, 8 Via Mike Bongiorno, 9 MILANO 20124 Sales Person : 2061971 ESPRI Purchse Group : QSW Payment Terms : S160 Purchaser : AAAAA Currency : EUR Plant : S425 Destination : CIF / MILANO [SEI]Plant TO SITE : Use Code : I Material Qty Price Per Unit Amount Delivery Material Description Commercial-Code 1 MI-SCBEXT36TH 1 7.41 1 PC 7.41 2020.06.29 MI-SCBEXT36TH,, MI-SCBEXT36TH Amount : 7.41 Remark 1 / 1";
+        var orderTxt = @"Purchase Order PO No : 5293274630 Date : 2020.03.16 Supplier Name & Address Buyer Name & Address L10IU1 : ECONOCOM INTERNATIONAL ITALIA S.P.A Samsung Electronics Italia S.p.A Via Marcello Nizzoli, 8 Via Mike Bongiorno, 9 MILANO 20124 Sales Person : 2061971 ESPRI Purchse Group : QSW Payment Terms : S160 Purchaser : AAAAA Currency : EUR Plant : S425 Destination : CIF / MILANO [SEI]Plant TO SITE : Use Code : I Material Qty Price Per Unit Amount Delivery Material Description Commercial-Code 1 MI-SCBSMT24SM 10 45.62 1 PC 456.20 2020.03.18 MI-SCBSMT24SM,, MI-SCBSMT24SM 2 MI-SCBSML24SM 10 32.28 1 PC 322.80 2020.03.18 MI-SCBSML24SM,, MI-SCBSML24SM Amount : 779.00 Remark 1 / 1";
         var results = orderTxt.Split(new string[] { "Purchase Order" }, StringSplitOptions.None);
 
         foreach (string result in results)
@@ -128,15 +292,58 @@ public class Program
 
                 //Console.WriteLine(OrderItemRowOne.ToString().Trim());
 
-                string[] OrderItem = OrderItemRowOne.ToString().Trim().Split(' ');
+                string[] TestString = Regex.Split(OrderItemRowOne.ToString(), @"(?<=\s|^)([0-9]+)(?=[\s]+[A-Z]*[-]+[A-Z0-9]*)");
 
-                orderRow.ArticleCode = OrderItem[1];
-                orderRow.Quantity = Convert.ToInt32(OrderItem[2]);
-                orderRow.Price = OrderItem[3];
-                orderRow.Amount = OrderItem[6];
-                orderRow.DeliveryDate = OrderItem[7].Replace(".", "-");
 
-                List.Add(orderRow);
+                for (int i = 0; i < TestString.Length; i++)
+                {
+                    if (TestString[i] == " " || TestString[i] == null || TestString[i].Count() > 1)
+                    {
+                        continue;
+                    }
+
+                    if (TestString[i] == "1")
+                    {
+                        string[] OrderItemOne = TestString[i + 1].Trim().Split(' ');
+
+                        orderRow.ArticleCode = OrderItemOne[0];
+                        orderRow.Quantity = Convert.ToInt32(OrderItemOne[1]);
+                        orderRow.Price = OrderItemOne[2];
+                        orderRow.Amount = OrderItemOne[5];
+                        orderRow.DeliveryDate = OrderItemOne[6].Replace(".", "-");
+
+                        List.Add(orderRow);
+
+                    }
+
+                    if (TestString[i] == "2")
+                    {
+                        orderRow = new SamsungOrderRow();
+
+                        string[] OrderItemTwo = TestString[i + 1].Trim().Split(' ');
+
+                        orderRow.ArticleCode = OrderItemTwo[0];
+                        orderRow.Quantity = Convert.ToInt32(OrderItemTwo[1]);
+                        orderRow.Price = OrderItemTwo[2];
+                        orderRow.Amount = OrderItemTwo[5];
+                        orderRow.DeliveryDate = OrderItemTwo[6].Replace(".", "-");
+
+                        List.Add(orderRow);
+
+                    }
+
+                }
+
+
+                //string[] OrderItem = OrderItemRowOne.ToString().Trim().Split(' ');
+
+                //orderRow.ArticleCode = OrderItem[1];
+                //orderRow.Quantity = Convert.ToInt32(OrderItem[2]);
+                //orderRow.Price = OrderItem[3];
+                //orderRow.Amount = OrderItem[6];
+                //orderRow.DeliveryDate = OrderItem[7].Replace(".", "-");
+
+                //List.Add(orderRow);
 
 
                 orderRow.getSetOrderRow = List;
